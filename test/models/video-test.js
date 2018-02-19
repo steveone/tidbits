9e6c993152ec9de8fd4e0da0bff8f08e92856977
steveone <steveone@gmail.com>
2018-02-18 18:01:23 -0800

    Added model test to ensure title for new videos is a string

--- test/models/video-test.js
+++ test/models/video-test.js
@@ -1,5 +1,6 @@
 const {assert} = require('chai');
 const {mongoose, databaseUrl, options} = require('../../database');
+const Video = require('../../models/video');
 
 async function connectDatabase() {
   await mongoose.connect(databaseUrl, options);
@@ -9,6 +10,21 @@ async function connectDatabase() {
 async function disconnectDatabase() {
   await mongoose.disconnect();
 }
+
+
+describe('very that the video title requires a string',() =>{
+  describe('testing the video title as an integer becomes a string',()=>{
+    it('Tries to submit a video that with an integer for a title updates to a string',()=>{
+        const newVideo = new Video({
+          title: 5,
+          description: 'testing',
+          url: 'https://www.youtube.com'
+        });
+        assert.strictEqual('5',newVideo.title);
+    })
+  })
+});
+
 
 module.exports = {
   connectDatabase,
