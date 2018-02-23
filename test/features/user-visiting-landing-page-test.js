ca53665d7e3ede30fe160938c9b9f6e5534aabb5
steveone <steveone@gmail.com>
2018-02-22 21:45:44 -0800

    Not quite working test to find iframe tag in index page

--- test/features/user-visiting-landing-page-test.js
+++ test/features/user-visiting-landing-page-test.js
@@ -1,5 +1,18 @@
 const {assert} = require('chai');
 
+const {connectDatabaseAndDropData, disconnectDatabase} = require('../database-utilities');
+
+
+  beforeEach(connectDatabaseAndDropData);
+
+  afterEach(disconnectDatabase);
+
+const video = {
+  title: 'Unit testing',
+  description: 'What is Unit Testing',
+  url: 'https://www.youtube.com/watch?v=lj5nnGa_DIw'
+}
+
 describe(`User visits landing page`,()=>{
   describe (`user visits landing page when it's empty`,()=>{
     it('no videos are shown',()=>{
@@ -11,13 +24,9 @@ describe(`User visits landing page`,()=>{
   })
 
   describe (`user visits landing page and videos show`,()=>{
-    it('are shown on landing page',()=>{
+    it('videos are shown on landing page',()=>{
       //get main landindg page
-      const video = {
-        title: 'Unit testing',
-        description: 'What is Unit Testing',
-        url: 'https://www.youtube.com/watch?v=lj5nnGa_DIw'
-      }
+
       browser.url('/videos/create');
       browser.setValue('#title-input', video.title);
       browser.setValue('#description-input', video.description);
@@ -25,8 +34,22 @@ describe(`User visits landing page`,()=>{
       browser.click('#submit-button');
       browser.url("/")
       assert.equal(browser.getText('.video-title'),video.title);
-
     })
+
+    it('vidoes are shown in iframe', ()=>{
+      //get main landindg page
+
+      browser.url('/videos/create');
+      browser.setValue('#title-input', video.title);
+      browser.setValue('#description-input', video.description);
+      browser.setValue('#url-input', video.url);
+      browser.click('#submit-button');
+      browser.url("/")
+      assert.equal(browser.element('iframe'),video.url);
+    })
+
   })
+
+
 
 })
